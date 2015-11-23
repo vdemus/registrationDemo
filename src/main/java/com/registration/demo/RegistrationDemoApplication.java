3762d6937ce85b7f2131ea9c01f0ac88908b17ed
vdemus <vitaliy.demus@gmail.com>
2015-11-23 07:42:01 +0200

    project structure

---
+++ src/main/java/com/registration/demo/RegistrationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.registration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationDemoApplication.class, args);
+    }
+}
